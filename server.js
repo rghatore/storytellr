032e652a4286fcd9019d17d972072ca00c27c775
Sebastian Kann <sebastiankann@gmail.com>
2020-10-19 14:03:36 +0000

    wrote sass skeleton

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 const express = require("express");
 const bodyParser = require("body-parser");
-const sass = require("node-sass-middleware");
+//const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require("morgan");
 
