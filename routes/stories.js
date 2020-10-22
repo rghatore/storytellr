203f3a326c45bf6b8b96cef4c230dbcc213fde8f
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-10-22 20:47:33 +0000

    voting data sent to client works

--- routes/stories.js
+++ routes/stories.js
@@ -4,6 +4,7 @@ const express = require("express"); // move this into server eventually
 const router = express.Router(); // move this into server enentually and pass as an argument
 const cookieSession = require("cookie-session");
 const { branchFilterAndSort, populateKeywordArray } = require("../helpers");
+const { promise } = require("bcrypt/promises");
 router.use(
   cookieSession({
     name: "session",
@@ -181,7 +182,7 @@ module.exports = (database) => {
 // });
 
 router.get("/branches/:branch_point_id", (req, res) => {
-  console.log(req.params)
+  // console.log(req.params)
   database
     .getBranchesByBranchPointId(req.params.branch_point_id)
     .then((branches) => {
@@ -199,12 +200,78 @@ router.get("/branches/:branch_point_id", (req, res) => {
             branch['owner'] = false;
           }
         }
-
-        res.send(branches);
+        // console.log('first then; checking if user logged in is the owner of the story ', branches)
+        return branches  // new
+        // res.send(branches); // working
       }
     })
+    //
+    // })
+    .then(branches => {
+      // console.log('receieved from first then: ', branches); // working until here
+      const loggedInUser = req.session['user_id'];
+      // console.log('branches inside final then: ', branches);
+      let vote = {user_id: loggedInUser}
+      // loop through branches array
+      // --
+      Promise.all(branches.map((branch) => {
+        vote['branchId'] = branch.id;
+        return database.checkVote(vote);
+        // let value = database.checkVote(vote)
+        // if (!value) {
+        //   value = {up: false};
+        //   return value;
+        // } else {
+        //   return value;
+        // }
+      }))
+        .then(data => {
+            console.log('response data: ', data)
+            console.log('branches :', branches)
+            //  data can be empty array with length 0
+            //  for loop wouldn't run
+            //  we're getting an array [undefined]
+            for (const item in data) {
+              console.log('item: ', item); //data.up at index 0 = branches.userVote at index 0
+              if (!data[item]) {
+                branches[item]['userVote'] = false;
+              } else if (!data[item].up) {
+                branches[item]['userVote'] = false;
+              } else {
+                branches[item]['userVote'] = true;
+              }
+            }
+            console.log('final branches: ', branches);
+            res.send(branches);
+        })
+        .catch(error => console.log(error))
+      // --
+      // for (const branch of branches) {
+      //   console.log('vote: ', vote)
+      //   // check each branch object
+      //   .then(data => {
+      //   // const data = database.checkVoteSync(vote);
+      //   console.log('user vote data :', data);
+      //     if (data) {
+      //       if(data.up) {
+      //         // user has currently upvoted this post
+      //         branch['userVote'] = true;
+      //       } else if (data.up === null) {
+      //         // user has currently unvoted this post
+      //         branch['userVote'] = false;
+      //       }
+      //     } else {
+      //       // user hasnt currently voted on this post
+      //       branch['userVote'] = false;
+      //     }
+      //     console.log('branch: ', branch)
+      //   })
+      // }
+      // console.log('branches sent to client side: ', branches);
+      // res.send(branches);
     })
-    .catch((error) => res.send(error.message));
+  })
+  .catch((error) => res.send(error.message));
 });
 
   router.put("/branches", (req, res) => {
