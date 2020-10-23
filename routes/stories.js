952d284a206b76751fac7848a61059e7c0c06b3c
Sebastian Kann <sebastiankann@gmail.com>
2020-10-23 15:24:13 +0000

    life or death

--- routes/stories.js
+++ routes/stories.js
@@ -72,8 +72,17 @@ module.exports = (database) => {
                     // branchFilterAndSort is defined in helpers.js on the server side
                     let approvedBranches = branchFilterAndSort(branches);
                     story[0].branches = approvedBranches;
-                    story[0].cookie = req.session["user_id"] ? true : false;
-                    res.send(story);
+                    const storyInfo = {
+                      storyTitle: story[0].title,
+                      storyOwner: story[0].user_name,
+                    };
+                    database
+                      .getBranchPointFromStoryPage(storyInfo)
+                      .then((openBranchPointId) => {
+                        story[0].open_branch_point = openBranchPointId.id;
+                        story[0].cookie = req.session["user_id"] ? true : false;
+                        res.send(story);
+                      });
                   });
               });
           });
